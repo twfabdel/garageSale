cff85d9ea1e74f7db8fa23d5899b9c3e151b5431
Tarek Abdelghany <tabdelghany@Tareks-MacBook-Pro.local>
2017-11-18 17:07:28 -0600

    map search

--- GarageSale/Map View/MapSearchTableViewController.swift
+++ GarageSale/Map View/MapSearchTableViewController.swift
@@ -7,11 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
 class MapSearchTableViewController: UITableViewController, UISearchResultsUpdating {
     
+    var mapView: MKMapView?
+    var searchResultList = [MKMapItem]()
+    
+    /* Update Search controller code adapted from
+     * https://www.thorntech.com/2016/01/how-to-search-for-location-using-apples-mapkit/
+     */
     func updateSearchResults(for searchController: UISearchController) {
-        print("change?")
+        guard let mapView = mapView,
+            let searchBarText = searchController.searchBar.text else { return }
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        search.start { response, _ in
+            guard let response = response else {
+                return
+            }
+            self.searchResultList = response.mapItems
+            self.tableView.reloadData()
+        }
     }
     
 
@@ -33,24 +52,49 @@ class MapSearchTableViewController: UITableViewController, UISearchResultsUpdati
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return searchResultList.count
+        default:
+            break
+        }
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MapSearchResultCell", for: indexPath)
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = "Current Location"
+        } else {
+            let placemark = searchResultList[indexPath.row].placemark
+            cell.textLabel?.text = placemark.name ?? placemark.title ?? "?"
+        }
 
         return cell
     }
-    */
+ 
+    
+    var locationSelectionClosure: ((MKPlacemark?, Bool) -> Void)?
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            locationSelectionClosure?(nil, true)
+        case 1:
+            let placemark = searchResultList[indexPath.row].placemark
+            locationSelectionClosure?(placemark, false)
+        default:
+            break
+        }
+    }
 
     /*
     // Override to support conditional editing of the table view.
--- GarageSale/Map View/SaleMapViewController.swift
+++ GarageSale/Map View/SaleMapViewController.swift
@@ -19,12 +19,22 @@ class SaleMapViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        map.showsUserLocation = true
+        
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestAlwaysAuthorization()
         manager.startUpdatingLocation()
         setMapLocationToUser()
-        
+
+        initializeSearchTable()
+    }
+    
+    
+    /* Search table design adapted from
+     * https://www.thorntech.com/2016/01/how-to-search-for-location-using-apples-mapkit/
+     */
+    private func initializeSearchTable() {
         if let saleSearchTable = storyboard?.instantiateViewController(withIdentifier: "MapSearchTable") as? MapSearchTableViewController {
             resultSearchController = UISearchController(searchResultsController: saleSearchTable)
             resultSearchController.searchResultsUpdater = saleSearchTable
@@ -36,22 +46,35 @@ class SaleMapViewController: UIViewController, CLLocationManagerDelegate {
             navigationItem.titleView = resultSearchController?.searchBar
             resultSearchController.hidesNavigationBarDuringPresentation = false
             resultSearchController.dimsBackgroundDuringPresentation = true
-//            definesPresentationContext = true
+            definesPresentationContext = true
             
+            saleSearchTable.mapView = map
+            saleSearchTable.locationSelectionClosure = setMapLocationTo(_:user:)
         }
-        
-       
     }
     
     // MARK: - Map and location
     
     func setMapLocationToUser() {
         if let location = manager.location {
-            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
             let locationCoordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
             
             map.setRegion(MKCoordinateRegion(center: locationCoordinate, span: span), animated: true)
-            map.showsUserLocation = true
+        }
+    }
+    
+    func setMapLocationTo(_ placemark: MKPlacemark?, user isUser: Bool) {
+        resultSearchController.dismiss(animated: true, completion: nil)
+        resultSearchController.isActive = false
+        if isUser {
+            setMapLocationToUser()
+        } else {
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            if let center = placemark?.location?.coordinate {
+                let region = MKCoordinateRegion(center: center, span: span)
+                map.setRegion(region, animated: true)
+            }
         }
     }
 }
