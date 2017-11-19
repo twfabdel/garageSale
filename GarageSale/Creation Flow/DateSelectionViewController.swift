0fededf80145789190b547b7a4f2602cd6890c74
Tarek Abdelghany <tabdelghany@Tareks-MacBook-Pro.local>
2017-11-19 09:09:19 -0600

    split date and location

--- GarageSale/Creation Flow/DateSelectionViewController.swift
+++ GarageSale/Creation Flow/DateSelectionViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class NewSaleViewController: UIViewController, UITextFieldDelegate {
+class DateSelectionViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var address: UITextField! {
         didSet {
