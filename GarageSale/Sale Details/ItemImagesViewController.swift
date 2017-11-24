9ee4274f00bfd44c16854b98b305d8567e59e0c1
twfabdel <twfabdel@gmail.com>
2017-11-24 11:36:54 -0800

    re-adding in detail views

---
+++ GarageSale/Sale Details/ItemImagesViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ItemImagesViewController.swift
+//  GarageSale
+//
+//  Created by Tarek Abdelghany on 11/24/17.
+//  Copyright © 2017 Tarek Abdelghany. All rights reserved.
+//
+
+import UIKit
+
+class ItemImagesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    var sale: SaleModel?
+    
+    @IBOutlet weak var itemImagesCollectionView: UICollectionView! {
+        didSet {
+            itemImagesCollectionView.delegate = self
+            itemImagesCollectionView.dataSource = self
+        }
+    }
+    
+    // MARK: - CollectionView DataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sale?.items?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = itemImagesCollectionView.dequeueReusableCell(withReuseIdentifier: "SaleDetailCell", for: indexPath)
+        
+        if let detailCell = cell as? SaleDetailsCollectionViewCell,
+            let itemsSet = sale?.items,
+            let item = itemsSet.allObjects[indexPath.row] as? ItemModel,
+            let itemImageData = item.image
+        {
+            detailCell.itemPriceLabel.text = item.price.priceString
+            detailCell.itemImageView.image = UIImage(data: itemImageData)
+        }
+        
+        return cell
+    }
+
+}
