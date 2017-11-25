71c4fb6c569ccc568649b1e93c7e1957a38a7fc4
twfabdel <twfabdel@gmail.com>
2017-11-24 18:26:11 -0800

    cleaned up individual image

--- GarageSale/Sale Details/SaleDetailsCollectionViewCell.swift
+++ GarageSale/Sale Details/SaleDetailsCollectionViewCell.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class SaleDetailsCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var itemPriceLabel: UILabel!
+
     
     func setImageView(with imgData: Data) {
         guard let image = UIImage(data: imgData)
             else { return }
         
-        let background = UIImageView(frame: self.frame)
-        background.image = image
-        background.clipsToBounds = true
-        background.contentMode = .scaleAspectFill
-        background.alpha = 0.5
-        addSubview(background)
-        sendSubview(toBack: background)
-        
         itemImageView.image = image
         itemImageView.contentMode = .scaleAspectFit
+        
+        backgroundImageView.image = image
+        backgroundImageView.clipsToBounds = true
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.alpha = 0.25
+        sendSubview(toBack: backgroundImageView)
     }
 }
